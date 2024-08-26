7025b788ca60445729456f4b68d8091632290b95
Dhyey <dhyeykakadiya1015@gmail.com>
2024-08-26 11:41:12 +0530

    added basic app view and urls

--- drf/backend/api/views.py
+++ drf/backend/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def api_home(request):
+    
+    return JsonResponse({"message" : "hi this the django api response"})
+
