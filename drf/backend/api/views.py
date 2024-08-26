a439d1459d75c2cc0a4a5492cebeb005d2b16820
Dhyey <dhyeykakadiya1015@gmail.com>
2024-08-26 12:45:38 +0530

    complete till manually send and reciving data

--- drf/backend/api/views.py
+++ drf/backend/api/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
 
 # Create your views here.
 
 def api_home(request):
     
-    return JsonResponse({"message" : "hi this the django api response"})
+    print(request.GET)
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    # data['headers'] = request.headers
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type 
+    data['params'] = dict(request.GET)
+    return JsonResponse(data)
 
--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -2,11 +2,13 @@ import requests
 
 # endpoint = "https://httpbin.org"
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://localhost:8000/"
+endpoint = "http://localhost:8000/api/"
 
 
-get_response = requests.get(endpoint, json= {"xyz" : "hello world!"})
-print(get_response.text)
-print(get_response.status_code)
-
-# print(get_response.json())
+get_response = requests.get(endpoint, params={"abc" : 123}, json= {"query" : "hello world!"})
+#print(get_response.text)
+#print(get_response.status_code)
+try:
+    print(get_response.json())
+except:
+     print("The response is not in JSON format.")
