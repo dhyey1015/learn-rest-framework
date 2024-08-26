46fc2091a9c216a9efc6a0fee50daaf63952111c
Dhyey <dhyeykakadiya1015@gmail.com>
2024-08-26 16:12:32 +0530

    completed basic of post request in rest framework

--- drf/backend/api/views.py
+++ drf/backend/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-# from django.http import JsonResponse, HttpResponse
+from django.http import JsonResponse, HttpResponse
 from products.models import *
 import json
 from django.forms.models import model_to_dict
@@ -8,21 +8,26 @@ from products.serializers import ProductSerializer
 ################ rest framework ######################
 
 from rest_framework.response import Response
+
 from rest_framework.decorators import api_view
 
 
 # Create your views here.
-@api_view(["GET"])
+@api_view(['POST'])
 def api_home(request):
     """
     DRF api view
     """
-    instance = Product.objects.all().order_by("?").first()
-    data = {}
-    if instance:
-        # data = model_to_dict(instance, fields= ['id', 'title', 'price', 'sale_price'])
-        data = ProductSerializer(instance).data
-        
-        return Response(data)
+    data = request.data
+    # instance = Product.objects.all().order_by("?").first()
+    # data = {}
+    # if instance:
+    #     # data = model_to_dict(instance, fields= ['id', 'title', 'price', 'sale_price'])
+    #     data = ProductSerializer(instance).data
+    serializer = ProductSerializer(data = request.data)
+    if serializer.is_valid():
+        instance = serializer.save()
+        print(instance)
+        return Response(serializer.data)
     # return HttpResponse(data, headers = {"Content-Type" : "application/json"})
 
