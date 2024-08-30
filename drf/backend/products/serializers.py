3491c40e463410d66864e2239c6c6b722dc55a25
Dhyey <dhyeykakadiya1015@gmail.com>
2024-08-30 23:50:15 +0530

    learned custom validation using serializers

--- drf/backend/products/serializers.py
+++ drf/backend/products/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from rest_framework.reverse import  reverse 
-
+from .validators import validate_title, validate_title_no_hello, unique_product_title
 
 from .models import Product
 
@@ -14,8 +14,12 @@ class ProductSerializer(serializers.ModelSerializer):
         lookup_field ='pk'
     )
     
-    email = serializers.EmailField(write_only = True)
-
+    # email = serializers.EmailField(write_only = True)
+    
+    #second way to do validate the fields in serializer validate_title is comming from validators.py 
+    title = serializers.CharField(validators=[unique_product_title, validate_title_no_hello])
+    #name = serializers.CharField(source = 'title', read_only=True) we can use foregionkey too
+     
     
     class Meta:
         model = Product
@@ -23,25 +27,36 @@ class ProductSerializer(serializers.ModelSerializer):
             'preffered_url',
             'url',
             'edit_url',
-            'email',
             'pk',
             'title',
+            # 'user',
             'content',
             'price',
             'sale_price',
             'discount',
             
         ]
-    #to create model by overwriting the default method to create a model 
-    def create(self, validated_data):
-        # email = validated_data.pop('email')
-        obj = super().create(validated_data)
-        # print(email, obj)
-        return obj
+    #one way to do validate the fields in serializer  we will use it here when we are using context.get 
+     
+    # def validate_title(self, value):
+    #     request =  self.context.get('request')
+    #     user = request.user
+    #     qs = Product.objects.filter(title__iexact=value)
+    #     if qs.exists():
+    #         raise   serializers.ValidationError(f"{value} is already product name")
+    #     return value    
+        
     
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title')
-        return instance
+    # #to create model by overwriting the default method to create a model 
+    # def create(self, validated_data):
+    #     # email = validated_data.pop('email')
+    #     obj = super().create(validated_data)
+    #     # print(email, obj)
+    #     return obj
+    
+    # def update(self, instance, validated_data):
+    #     instance.title = validated_data.get('title')
+    #     return instance
     
     #one way to use reverse    
     def get_url(self, obj):
--- drf/backend/products/views.py
+++ drf/backend/products/views.py
@@ -48,8 +48,6 @@ class ProductListCreateAPIView(
     serializer_class = ProductSerializer 
     
     def perform_create(self, serializer):
-        email = serializer.validated_data.pop('email')
-        print(email)
         title = serializer.validated_data.get('title')
         content = serializer.validated_data.get('content')or None
         if content is None:
