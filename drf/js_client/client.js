812f73f344e2064112599e3b051357bd41b45d6d
dhyey1015 <dhyeykakadiya1015@gmail.com>
2024-09-05 22:01:25 +0530

    learned to use JWT with JS client

--- drf/js_client/client.js
+++ drf/js_client/client.js
@@ -1,3 +1,4 @@
+const contentContainer = document.getElementById('content-container')
 const loginFrom = document.getElementById('login-form')
 const baseEndpoint = "http://127.0.0.1:8000/api"
 
@@ -27,17 +28,64 @@ function handleLogin(event){
     fetch(loginEndpoint, options)
 
     .then(response =>{
-        console.log(response)
         return response.json()
     })
 
-    .then(x =>{
-        console.log(x)
+    .then(authData => {
+        handleAuthData(authData, getProductList)
     })
 
     .catch(error =>{
         console.log(error)
     }
-
     )
+}
+
+function handleAuthData(authData, callback){
+    localStorage.setItem('access', authData.access)
+    localStorage.setItem('refresh', authData.refresh)
+
+    if (callback){
+        callback()
+    }
+
+}
+
+function writeToContainer(data){
+    if (contentContainer){
+        contentContainer.innerHTML = "<pre>" + JSON.stringify(data, null, 4)+ "</pre>" 
+    }
+}
+
+function getFetchOptions(method, body){
+    return {
+        method : method === null ? "GET": method,
+        headers : {
+            "Content-Type" : "application/json",
+            "Authorization" : `Bearer ${localStorage.getItem('access')}`
+        },
+        body : body ? body : null
+    }
+}
+
+function isTokenNotvalid(jsonData){
+    if (jsonData.code && jsonData.code === "token_not_valid"){
+        alert("please login again")
+    }
+}
+
+function getProductList(){
+    const endpoint = `${baseEndpoint}/products/`
+    const options = getFetchOptions()
+    
+    fetch(endpoint, options)
+    .then(response => {
+        return response.json()
+    })
+    .then(data =>{
+        console.log(data)
+        const validData = isTokenNotvalid(data)
+        if (validData)
+        writeToContainer(data)
+    })
 }
